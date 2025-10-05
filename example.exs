5b83376e9ba1a8aa8bed457b54a4e6cce1e3cd57
Miles Frankel <miles.frankel@cockroachlabs.com>
2025-10-05 01:07:02 -0400

    ex

--- example.exs
+++ example.exs
@@ -3,16 +3,16 @@
 # :observer.start()
 :ok = Daftka.MetadataAPI.Server.create_topic("hi", 4)
 :ok = Daftka.MetadataAPI.Server.wait_for_online("hi", 0, 1000)
+
 {:ok, t} = Daftka.Types.new_topic("hi")
 {:ok, p} = Daftka.Types.new_partition(0)
-{:ok, prs} = Daftka.Metadata.Store.get_partition_owner(t, p)
 
 _ =
   Enum.map(1..10, fn x ->
-    {:ok, _offset} = Daftka.PartitionReplica.Server.append(prs, "k-#{x}", "v-#{x}")
+    {:ok, _offset} = Daftka.Router.produce(t, p, "k-#{x}", "v-#{x}")
   end)
 
 {:ok, o} = Daftka.Types.new_offset(0)
-{:ok, msgs} = Daftka.PartitionReplica.Server.fetch_from(prs, o, 5)
+{:ok, msgs} = Daftka.Router.fetch_from(t, p, o, 5)
 
 IO.inspect(msgs, pretty: true)
